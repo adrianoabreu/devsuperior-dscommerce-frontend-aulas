b29312db6e5cc9d8eb807625b122a8beb3dfb637
Adriano Abreu <adriano.luis.machado2013@hotmail.com>
2025-06-10 08:37:32 -0300

    ReactJS - Renderizando colecoes dinamicamente no React

--- src/components/ProductDetailsCard/index.tsx
+++ src/components/ProductDetailsCard/index.tsx
@@ -6,22 +6,24 @@ type Props = {
     product: ProductDTO;
 }
 
-export default function ProductDetailsCard ({ product } : Props){
+export default function ProductDetailsCard({ product }: Props) {
     return (
         <div className="dsc-card dsc-mb20">
-                <div className="dsc-product-details-top dsc-line-bottom">
-                    <img src={product.imgUrl} alt={product.name}/>
-                </div>
-                <div className="dsc-product-details-bottom">
-                    <h3>R$ {product.price.toFixed(2)}</h3>
-                    <h4>{product.name}</h4>
-                    <p>{product.description}</p>
-                    <div className="dsc-category-container">
-                        <ProductCategory name="Eletrônicos"/>
-                        <ProductCategory name="Computadores"/>
-                        
-                    </div>
+            <div className="dsc-product-details-top dsc-line-bottom">
+                <img src={product.imgUrl} alt={product.name} />
+            </div>
+            <div className="dsc-product-details-bottom">
+                <h3>R$ {product.price.toFixed(2)}</h3>
+                <h4>{product.name}</h4>
+                <p>{product.description}</p>
+                <div className="dsc-category-container">
+                    {
+                        product.categories.map(item => (
+                            <ProductCategory key={item.id} name={item.name} />
+                        ))
+                    }
                 </div>
             </div>
+        </div>
     );
 }
