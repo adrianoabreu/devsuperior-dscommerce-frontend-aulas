b41a3189d7802a4cd7e59641d1abdb60889d0cc5
Adriano Abreu <adriano.luis.machado2013@hotmail.com>
2025-06-12 08:51:10 -0300

    ReactJS - Adicionando Rotas com React Router DOM

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
-//import ProductDetails from './routes/ProductDetails';
-import Catalog from './routes/Catalog';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Catalog from './routes/ClientHome/Catalog';
+import ProductDetails from './routes/ClientHome/ProductDetails';
+import ClientHome from './routes/ClientHome';
 
 export default function App() {
   return (
-    //<ProductDetails/>
-    <Catalog/>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<ClientHome />}>
+        <Route index element={<Catalog/>}/>
+        <Route path="catalog" element={<Catalog/>}/>
+        <Route path="product-details" element={<ProductDetails/>}/>
+      </Route>
+    </Routes>
+  </BrowserRouter>
   );
 }
 
