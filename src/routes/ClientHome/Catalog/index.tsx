451297c6911b19e6567198fa5ba99b33b10a6a18
Adriano Abreu <adriano.luis.machado2013@hotmail.com>
2025-06-13 15:48:30 -0300

    ReactJS - Implementacao de requisicoes com useState, useEffect e Axios para a lista do catalogo de produtos e para a busca por id do produto

--- src/routes/ClientHome/Catalog/index.tsx
+++ src/routes/ClientHome/Catalog/index.tsx
@@ -3,17 +3,28 @@ import SearchBar from '../../../components/SearchBar';
 import CatalogCard from '../../../components/CatalogCard';
 import ButtonNextPage from '../../../components/ButtonNextPage';
 import type { ProductDTO } from '../../../models/product';
-import * as productService from '../../../services/product-service';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 
 export default function Catalog() {
+  
+  const [products, setProducts] = useState<ProductDTO[]>([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:8080/products?size=12")
+        .then(response => {
+          setProducts(response.data.content);
+        });
+  }, []);
+  
   return (
     <main>
       <section id="catalog-section" className="dsc-container">
         <SearchBar />
         <div className="dsc-catalog-cards dsc-mb20 dsc-mt20">
           {
-            productService.findAll().map(product => <CatalogCard key={product.id} product={product} />)
+            products.map(product => <CatalogCard key={product.id} product={product} />)
           }
         </div>
         <ButtonNextPage />
