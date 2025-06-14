a8a9f7ca48bfd1b1f8bbac4c4735550a0a2a80b7
Adriano Abreu <adriano.luis.machado2013@hotmail.com>
2025-06-14 18:37:27 -0300

    ReactJS - Adicionando produtos ao carrinho

--- src/routes/ClientHome/ProductDetails/index.tsx
+++ src/routes/ClientHome/ProductDetails/index.tsx
@@ -7,6 +7,7 @@ import { Link } from 'react-router-dom';
 import type { ProductDTO } from '../../../models/product';
 import { useEffect, useState } from 'react';
 import * as productService from '../../../services/product-service';
+import * as cartService from '../../../services/cart-service';
 
 export default function ProductDetails() {
 
@@ -27,6 +28,13 @@ export default function ProductDetails() {
       });  
   }, []);
   
+function handleBuyClick() {
+  if(product){
+    cartService.addProduct(product);
+    navigate("/cart");
+  }
+}
+
   return (
     <main>
       <section id="product-details-section" className="dsc-container">
@@ -36,7 +44,9 @@ export default function ProductDetails() {
         }
 
         <div className="dsc-btn-page-container">
-          <ButtonPrimary text="Comprar" />
+          <div onClick={handleBuyClick}>
+            <ButtonPrimary text="Comprar" />
+          </div>
           <Link to="/">
             <ButtonInverse text="Inicio" />
           </Link>
