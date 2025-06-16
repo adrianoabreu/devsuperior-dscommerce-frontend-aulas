362397c7ebda8917adce64dce4ef9f22e18056e7
Adriano Abreu <adriano.luis.machado2013@hotmail.com>
2025-06-16 16:01:34 -0300

    ReactJS - Buscando os dados do localStorage para notificacao do carrinho parte 1

--- src/components/CartIcon/index.tsx
+++ src/components/CartIcon/index.tsx
@@ -1,11 +1,16 @@
+import { useState } from 'react';
 import cartIcon from '../../assets/cart.svg';
+import * as cartService from '../../services/cart-service';
 import './styles.css';
 
 export default function CartIcon() {
+
+    const [cart, setCart] = useState(cartService.getCart());
+
     return (
         <>
             <img src={cartIcon} alt="Carrinho de compras" />
-            <div className="dsc-cart-count">2</div>
+            <div className="dsc-cart-count">{cart.items.length}</div>
         </>
     );
 }
