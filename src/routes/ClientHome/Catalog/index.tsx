bcca3b4092726023dbebc264cf8ee6d9fda19ef7
Adriano Abreu <adriano.luis.machado2013@hotmail.com>
2025-06-16 12:14:39 -0300

    ReactJS - Implementando botao de carregar mais proxima pagina de produtos na tela de catalogo

--- src/routes/ClientHome/Catalog/index.tsx
+++ src/routes/ClientHome/Catalog/index.tsx
@@ -13,6 +13,8 @@ type QueryParams = {
 
 export default function Catalog() {
   
+  const [isLastPage, setIsLastPage] = useState(false);
+
   const [products, setProducts] = useState<ProductDTO[]>([]);
 
   const [queryParams, setQueryParam] = useState<QueryParams>({
@@ -23,12 +25,19 @@ export default function Catalog() {
   useEffect(() => {
     productService.findPageRequest(queryParams.page, queryParams.name)
         .then(response => {
-          setProducts(response.data.content);
+          const nextPage = response.data.content;
+          setProducts(products.concat(nextPage));
+          setIsLastPage(response.data.last);
         });
   }, [queryParams]);
   
   function handleSearch(searchText: string) {
-    setQueryParam({...queryParams, name: searchText});
+    setProducts([]);
+    setQueryParam({...queryParams, page: 0, name: searchText});
+  }
+
+  function handleNextPageClick(){
+    setQueryParam({...queryParams, page: queryParams.page + 1});
   }
 
   return (
@@ -40,7 +49,14 @@ export default function Catalog() {
             products.map(product => <CatalogCard key={product.id} product={product} />)
           }
         </div>
-        <ButtonNextPage />
+          {//tratamento para renderizar botão quando não for a ultima pagina
+            !isLastPage &&
+            <div onClick={handleNextPageClick}>
+              <ButtonNextPage />
+            </div>
+          }
+
+        
       </section>
     </main>
 
