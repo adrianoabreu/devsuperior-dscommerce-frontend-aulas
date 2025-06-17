f498d1a8670847b06b3103f084bdcfe712fbb190
Adriano Abreu <adriano.luis.machado2013@hotmail.com>
2025-06-17 18:35:54 -0300

    ReactJS - adicionando Interceptors do Axios ao projeto

--- src/utils/requests.ts
+++ src/utils/requests.ts
@@ -4,13 +4,42 @@ import axios from "axios";
 import * as authService from '../services/auth-services';
 
 export function requestBackend(config: AxiosRequestConfig) {
-    
-   const headers = config.withCredentials
-    ? {
-        ...config.headers,
-        Authorization: "Bearer " + authService.getAccessToken()
-    }
-    : config.headers;
-    
-    return axios({...config, baseURL: BASE_URL , headers});
+
+    const headers = config.withCredentials
+        ? {
+            ...config.headers,
+            Authorization: "Bearer " + authService.getAccessToken()
+        }
+        : config.headers;
+
+    return axios({ ...config, baseURL: BASE_URL, headers });
 }
+
+// REQUEST INTERCEPTOR
+axios.interceptors.request.use(
+    function (config) {
+        // DO SOMETHING BEFORE REQUEST IS SENT
+        return config;
+    },
+    function (error) {
+        // DO SOMETHING WITH REQUEST ERROR
+        return Promise.reject(error);
+    }
+);
+
+// RESPONSE INTERCEPTOR
+axios.interceptors.response.use(
+    function (response) {
+        // DO SOMETHING WITH RESPONSE DATA IF STATUS IS 2xx
+        return response;
+    },
+    function (error) {
+        if(error.response.status === 401) {
+            console.log("DEU 401!!!");
+        }
+        if(error.response.status === 403) {
+            console.log("DEU 403!!!");
+        }
+        return Promise.reject(error);
+    }
+);
