3402a36a97a210e7b67b23b83edbafa2ffa1ffd8
Adriano Abreu <adriano.luis.machado2013@hotmail.com>
2025-06-18 20:06:48 -0300

    ReactJS - Atualizando o PrivateRoute para controle de acesso por perfil de usuario em nivel de rota

--- src/components/PrivateRoute/index.tsx
+++ src/components/PrivateRoute/index.tsx
@@ -1,16 +1,21 @@
 import type { JSX } from "react";
 import { Navigate } from "react-router-dom";
 import * as authService from '../../services/auth-services';
+import type { RoleEnum } from "../../models/auth";
 
 type Props = {
     children: JSX.Element;
+    roles?: RoleEnum[];
 }
 
 //Função para redirecionar para tela de login quando usuário não estiver autenticado.
 //Ou seja, token expirado. Não faz acesso ao backend.
-export function PrivateRoute({ children }: Props) {
+export function PrivateRoute({ children , roles = [] }: Props) {
     if (!authService.isAuthenticated()) {
         return <Navigate to="/login" />;
+    }
+    if (!authService.hasAnyRoles(roles)) {
+        return <Navigate to="/catalog" />;
     }
     return children;
 }
