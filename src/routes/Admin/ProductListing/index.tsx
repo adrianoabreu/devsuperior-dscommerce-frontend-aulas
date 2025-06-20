021cda1fc5f60dcee2d13f259ce553253f85aae9
Adriano Abreu <adriano.luis.machado2013@hotmail.com>
2025-06-20 18:11:03 -0300

    ReactJS - Programando a dialog modal de aviso na tela de listagem de produtos

--- src/routes/Admin/ProductListing/index.tsx
+++ src/routes/Admin/ProductListing/index.tsx
@@ -15,6 +15,11 @@ type QueryParams = {
 
 export default function ProductListing() {
 
+    const [dialogInfoData, setDialogInfoData] = useState({
+        visible: false,
+        message: "Operação realizada com sucesso!"
+    });
+
     const [isLastPage, setIsLastPage] = useState(false);
 
     const [products, setProducts] = useState<ProductDTO[]>([]);
@@ -42,6 +47,14 @@ export default function ProductListing() {
         setQueryParam({ ...queryParams, page: queryParams.page + 1 });
     }
 
+    function handleDialogInfoClose() {
+        setDialogInfoData({ ...dialogInfoData, visible: false });
+    }
+
+    function handleDeleteClick() {
+        setDialogInfoData({ ...dialogInfoData, visible: true });
+    }
+
     return (
         <main>
             <section id="product-listing-section" className="dsc-container">
@@ -56,12 +69,14 @@ export default function ProductListing() {
 
                 <table className="dsc-table dsc-mb20 dsc-mt20">
                     <thead>
-                        <th className="dsc-tb576">ID</th>
-                        <th></th>
-                        <th className="dsc-tb768">Preço</th>
-                        <th className="dsc-txt-left">Nome</th>
-                        <th></th>
-                        <th></th>
+                        <tr>
+                            <th className="dsc-tb576">ID</th>
+                            <th></th>
+                            <th className="dsc-tb768">Preço</th>
+                            <th className="dsc-txt-left">Nome</th>
+                            <th></th>
+                            <th></th>
+                        </tr>
                     </thead>
                     <tbody>
                         {
@@ -72,7 +87,7 @@ export default function ProductListing() {
                                     <td className="dsc-tb768">R$ {product.price.toFixed(2)}</td>
                                     <td className="dsc-txt-left">{product.name}</td>
                                     <td><img className="dsc-product-listing-btn" src={editIcon} alt="Editar" /></td>
-                                    <td><img className="dsc-product-listing-btn" src={deleteIcon} alt="Deletar" /></td>
+                                    <td><img onClick={handleDeleteClick} className="dsc-product-listing-btn" src={deleteIcon} alt="Deletar" /></td>
                                 </tr>
                             ))
                         }
@@ -84,7 +99,12 @@ export default function ProductListing() {
                     <ButtonNextPage onNextPage={handleNextPageClick} />
                 }
             </section>
-            <DialogInfo/>
+            {
+                dialogInfoData.visible &&
+                <DialogInfo
+                    message={dialogInfoData.message}
+                    onDialogClose={handleDialogInfoClose} />
+            }
         </main>
     );
 }
