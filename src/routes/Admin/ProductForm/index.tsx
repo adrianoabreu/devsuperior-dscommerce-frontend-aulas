1602e01ca53dc4c2bd906b834dcccf5cdec90c64
Adriano Abreu <adriano.luis.machado2013@hotmail.com>
2025-06-21 13:47:52 -0300

    ReactJS - Ajustando CSS para mostrar erros no formulario

--- src/routes/Admin/ProductForm/index.tsx
+++ src/routes/Admin/ProductForm/index.tsx
@@ -40,7 +40,9 @@ export default function ProductForm() {
     });
 
     function handleInputChange(event: any) {
-        setFormData(forms.update(formData, event.target.name, event.target.value));
+        const dataUpdated = forms.update(formData, event.target.name, event.target.value);
+        const dataValidated = forms.validate(dataUpdated, event.target.name);
+        setFormData(dataValidated);
     }
 
     //Recupera produto do backend para editar no formulario
@@ -66,6 +68,7 @@ export default function ProductForm() {
                                     className="dsc-form-control"
                                     onChange={handleInputChange}
                                 />
+                                <div className="dsc-form-error">{formData.name.message}</div>
                             </div>
                             <div>
                                 <FormInput
@@ -73,6 +76,7 @@ export default function ProductForm() {
                                     className="dsc-form-control"
                                     onChange={handleInputChange}
                                 />
+                                <div className="dsc-form-error">{formData.price.message}</div>
                             </div>
                             <div>
                                 <FormInput
