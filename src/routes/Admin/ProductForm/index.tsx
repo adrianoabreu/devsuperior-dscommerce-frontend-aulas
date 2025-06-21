e7030f9e21e7d8cba43e9a8e92e8858667a7c3f1
Adriano Abreu <adriano.luis.machado2013@hotmail.com>
2025-06-21 11:24:40 -0300

    ReactJS - Comecando a preparar o formulario de produtos

--- src/routes/Admin/ProductForm/index.tsx
+++ src/routes/Admin/ProductForm/index.tsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import './styles.css';
 
 export default function ProductForm() {
@@ -11,7 +12,6 @@ export default function ProductForm() {
                         <div className="dsc-form-controls-container">
                             <div>
                                 <input className="dsc-form-control" type="text" placeholder="Nome" />
-
                             </div>
                             <div>
                                 <input className="dsc-form-control" type="text" placeholder="Preço" />
@@ -35,7 +35,9 @@ export default function ProductForm() {
                         </div>
 
                         <div className="dsc-product-form-buttons">
-                            <button type="reset" className="dsc-btn dsc-btn-white">Cancelar</button>
+                            <Link to="/admin/products">
+                                <button type="reset" className="dsc-btn dsc-btn-white">Cancelar</button>
+                            </Link>
                             <button type="submit" className="dsc-btn dsc-btn-blue">Salvar</button>
                         </div>
                     </form>
--- src/routes/Admin/ProductListing/index.tsx
+++ src/routes/Admin/ProductListing/index.tsx
@@ -8,6 +8,8 @@ import SearchBar from '../../../components/SearchBar';
 import ButtonNextPage from '../../../components/ButtonNextPage';
 import DialogInfo from '../../../components/DialogInfo';
 import DialogConfirmation from '../../../components/DialogConfirmation';
+import ButtonInverse from '../../../components/ButtonInverse';
+import { useNavigate } from 'react-router-dom';
 
 type QueryParams = {
     page: number;
@@ -15,6 +17,8 @@ type QueryParams = {
 }
 
 export default function ProductListing() {
+
+    const navigate = useNavigate();
 
     const [dialogInfoData, setDialogInfoData] = useState({
         visible: false,
@@ -79,6 +83,10 @@ export default function ProductListing() {
         setDialogConfirmationData({ ...dialogConfirmationData, visible: false });
     }
 
+    function handleNewProductClick() {
+        navigate("/admin/products/create");
+    }
+
     return (
         <main>
             <section id="product-listing-section" className="dsc-container">
@@ -86,7 +94,9 @@ export default function ProductListing() {
                 <h2 className="dsc-section-title dsc-mb20">Cadastro de Produtos</h2>
 
                 <div className="dsc-btn-page-container dsc-mb20">
-                    <div className="dsc-btn dsc-btn-white">Novo</div>
+                    <div onClick={handleNewProductClick}>
+                        <ButtonInverse text="Novo" />
+                    </div>
                 </div>
 
                 <SearchBar onSearch={handleSearch} />
