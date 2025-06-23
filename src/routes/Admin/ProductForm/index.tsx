b005c9c1f2c01b3d12af37fec04554a9be7ff0e3
Adriano Abreu <adriano.luis.machado2013@hotmail.com>
2025-06-23 16:19:05 -0300

    ReactJS - Sujando e validando todos os campos programaticamente para submeter formulario de cadastro de produtos

--- src/routes/Admin/ProductForm/index.tsx
+++ src/routes/Admin/ProductForm/index.tsx
@@ -101,6 +101,12 @@ export default function ProductForm() {
 
     function handleSubmit(event: any) {
         event.preventDefault();
+
+        const formDataValidated = forms.dirtyAndValidateAll(formData);
+        if(forms.hasAnyInvalid(formDataValidated)) {
+            setFormData(formDataValidated);
+            return;
+        }
     }
 
     return (
--- src/utils/forms.ts
+++ src/utils/forms.ts
@@ -43,6 +43,52 @@ export function dirtyAndValidate(inputs: any, name: string) {
     return validate(dataDirty, name);
 }
 
+export function toDirtyAll(inputs: any) {
+    const newInputs: any = {};
+    for (var name in inputs) {
+        newInputs[name] = {...inputs[name], dirty: "true"};
+    }
+    return newInputs;
+}
+
+//Validando todos os campos do formulário.
+export function validateAll(inputs: any) {
+    const newInputs: any = {};
+    for (var name in inputs) {
+        if (inputs[name].validation) {
+            const isInvalid = !inputs[name].validation(inputs[name].value);
+            newInputs[name] = {...inputs[name], invalid: isInvalid.toString()};
+        }
+        else {
+            newInputs[name] = {...inputs[name]}
+        }
+    }
+    return newInputs;
+}
+
+export function dirtyAndValidateAll(inputs: any) {
+    return validateAll(toDirtyAll(inputs));
+}
+
+export function hasAnyInvalid(inputs: any) {
+    for(var name in inputs) {
+        if (inputs[name].dirty === "true" && inputs[name].invalid === "true") {
+            return true;
+        }
+    }
+    return false;
+}
+
+
+
+
+
+
+
+
+
+
+
 
 
 
